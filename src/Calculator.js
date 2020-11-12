5ce9fd4815b857ee216d55ec794d448864882161
Jessie Chen <yhcjessie1@gmail.com>
2020-11-12 01:11:43 -0500

    STEP3 Bind the inputs and trigger onChange

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,32 +8,9 @@ class Calculator extends Component {
       no2: 0,
       mySum: 0,
     };
+    //STEP 3 Bind the inputs
     this._changeNo1 = this._changeNo1.bind(this);
     this._changeNo2 = this._changeNo2.bind(this);
-  }
-  render() {
-    return (
-      <div className="container">
-        <h1>Add with React!</h1>
-
-        <div className="add">
-          <input
-            type="text"
-            value={this.state.no1}//STEP 2 Show 0
-            onChange={this._changeNo1}
-          />
-          <span>+</span>
-          <input
-            type="text"
-            value={this.state.no2}//STEP 2 Show 0
-            onChange={this._changeNo2}
-          />
-          <span>=</span>
-          {/* STEP 2 Show 0 */}
-          <h3>Result: {this.state.mySum}</h3> 
-        </div>
-      </div>
-    );
   }
   _changeNo1(e) {
     let newNo1 = +e.target.value;
@@ -49,6 +26,31 @@ class Calculator extends Component {
       mySum: newNo2 + this.state.no1,
     });
   }
+  render() {
+    return (
+      <div className="container">
+        <h1>Add with React!</h1>
+
+        <div className="add">
+          <input
+            type="text"
+            value={this.state.no1}//STEP 2 Show 0
+            onChange={this._changeNo1}//line 12 STEP 3 Bind and trigger onChange
+          />
+          <span>+</span>
+          <input
+            type="text"
+            value={this.state.no2}//STEP 2 Show 0
+            onChange={this._changeNo2}//line 12 STEP 3 Bind and trigger onChange
+          />
+          <span>=</span>
+          {/* STEP 2 Show 0 */}
+          <h3>Result: {this.state.mySum}</h3> 
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default Calculator;
